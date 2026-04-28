0906d37a775fafb65f7b186b3c27ce9d34e7c873
corahirst <cora.virginia.hirst@gmail.com>
2026-04-28 17:08:24 -0400

    adds secondary response model and renames functions

--- code/functions/functions.R
+++ code/functions/functions.R
@@ -15,7 +15,7 @@ M_s = function(A0, t_p, r, t) {A0/(1+exp(-r*(t-t_p)))} #sigmoidal growth, A0 is 
 #####################################################
 
 #michaelis menten kinetics - desolve ode model
-IgA_competition <- function(t, y, parms)  # Single partial immune class (RPS)                       
+IgA_competition.model <- function(t, y, parms)  # Single partial immune class (RPS)                       
 {with(as.list(parms),	# allows the parameter file parms to be as a list
       {  
         y = pmax(y,0)         # avoid negative values (not ideal but ok)
@@ -40,7 +40,7 @@ IgA_competition <- function(t, y, parms)  # Single partial immune class (RPS)
 } 
 
 #michaelis menten kinetics - desolve ode model
-total_transported <- function(t, y, parms)  # Single partial immune class (RPS)                       
+total_transported.model <- function(t, y, parms)  # Single partial immune class (RPS)                       
 {with(as.list(parms),	# allows the parameter file parms to be as a list
       {  
         y = pmax(y,0)         # avoid negative values (not ideal but ok)
@@ -68,4 +68,26 @@ total_transported <- function(t, y, parms)  # Single partial immune class (RPS)
 
 #####################################################
 # Antibody concentrations in the respiratory mucosa #
-#####################################################
+secondary_response.model <- function(t, y, parms)  # Single partial immune class (RPS)                       
+{with(as.list(parms),	# allows the parameter file parms to be as a list
+      {  
+        y = pmax(y,0)         # avoid negative values (not ideal but ok)
+        
+        # map the state variables
+        tot.IgA = y[1]         # specific Ab in the lumen
+        
+        # make empty variables for the derivatives
+        dtot.IgA = 0 #change in specific Ab in the lumen
+        
+        #####################################
+        #####################################
+        #	 calculate the derivatives
+        dtot.IgA = (M_s(A0, t_p, r, t) + M_ns(c = c, d = d_mu))*(Vmax/(K + M_s(A0, t_p, r, t) + M_ns(c = c, d = d_mu))) - d_l*tot.IgA
+        
+        # output the derivatives
+        dy=c(dtot.IgA) #vector of derivatives 
+        
+        return(list(dy)) #lists vector and returns
+      }
+) 
+} 
