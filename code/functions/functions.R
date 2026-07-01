44ee4b98a555e134580c7e706ec7cd1510e647c4
corahirst <cora.virginia.hirst@gmail.com>
2026-07-01 15:06:23 -0400

    Refine ODE model parameter derivations and simulation configuration
    Ensures consistency by explicitly including the `D` decay rate in `r0` and `alpha` calculations.
    Derives `RT` and `k1` from existing parameters, reducing arbitrary constants and linking them mechanically.
    Extends simulation runtimes (`Tmax`, `Tmax_ss`) to more accurately capture steady-state behaviors.
    Updates plotting to emphasize luminal antibody dynamics (`Ls`, `Lns`) as key outputs.
    Includes minor corrections to the `r(t)` function definition and steady-state initialization.

--- code/functions/functions.R
+++ code/functions/functions.R
@@ -18,7 +18,7 @@ primary_response.model <- function(t, y, parms)  # Single partial immune class (
         #####################################
         # define time dependent functions   #
         #####################################
-        r = function(t) {if(t < tp) {r0 - alpha*t}
+        r = function(t) {if(t <= tp) {r0 - alpha*t}
           else 0} #define time-varying r(t) - increase that slows and is then dominated by decay rate
         #####################################
         ## calculate the derivatives
@@ -102,7 +102,7 @@ IgA_competition.model <- function(t, y, parms)  # Single partial immune class (R
         dMs = r(t)*Ms - k1*Ms*Re - d_mu*Ms - D*Ms # specific Ab production and decay
         dMns = c - k1*Re*Mns - d_mu*Mns - D*Mns
         dRs = k1*Ms*Re - k2*Rs
-        dRns = k1*Mns*Re - k2*Rns
+        dRns = k1*Re*Mns - k2*Rns
         dLs = k2*Rs - d_L*Ls
         dLns = k2*Rns - d_L*Lns
         
